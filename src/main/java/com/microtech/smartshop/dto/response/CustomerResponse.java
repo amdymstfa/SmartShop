804d9858d0bdd7244b6fd48af1524c475bbabebe
Moustapha Ndiaye <mstfa.ndiaye23@gmail.com>
2025-11-25 23:20:29 +0100

    feat(customer): update CustomerResponse fields

--- src/main/java/com/microtech/smartshop/dto/response/CustomerResponse.java
+++ src/main/java/com/microtech/smartshop/dto/response/CustomerResponse.java
@@ -15,9 +15,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class CustomerResponse {
 
+    private Long id;
+    private String name;
+    private String email;
     private CustomerTier tier;
-
+    private Integer totalOrders;
     private BigDecimal totalSpent;
-
     private LocalDateTime firstOrderDate;
+    private LocalDateTime lastOrderDate;
+
 }
