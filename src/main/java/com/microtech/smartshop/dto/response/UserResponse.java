e01217646a5ab9db5c21e363f340625175b7184e
Moustapha Ndiaye <mstfa.ndiaye23@gmail.com>
2025-11-25 15:01:18 +0100

    feat: create UserResponse DTO for API output

---
+++ src/main/java/com/microtech/smartshop/dto/response/UserResponse.java
@@ -0,0 +1,20 @@
+package com.microtech.smartshop.dto.response;
+
+import com.microtech.smartshop.enums.UserRole;
+
+public class UserResponse {
+
+    private Long id ;
+
+    private String name ;
+
+    private String email;
+
+    private String userName;
+
+    private String address ;
+
+    private int phone ;
+
+    private UserRole role ;
+}
