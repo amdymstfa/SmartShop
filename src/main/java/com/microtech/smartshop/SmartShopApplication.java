5b7e64588e6428e45d2b0311ba07ae7753d17990
Moustapha Ndiaye <mstfa.ndiaye23@gmail.com>
2025-11-25 15:01:01 +0100

    feat: enable JPA auditing for createdAt and updatedAt fields

--- src/main/java/com/microtech/smartshop/SmartShopApplication.java
+++ src/main/java/com/microtech/smartshop/SmartShopApplication.java
@@ -2,8 +2,10 @@ package com.microtech.smartshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SmartShopApplication {
 
 	public static void main(String[] args) {
