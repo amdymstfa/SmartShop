296c536672ba4f19506f20692fcde69e2abfa225
Moustapha Ndiaye <mstfa.ndiaye23@gmail.com>
2025-11-25 15:01:14 +0100

    feat: create UserRequest DTO for API input

---
+++ src/main/java/com/microtech/smartshop/dto/request/UserRequest.java
@@ -0,0 +1,21 @@
+package com.microtech.smartshop.dto.request;
+
+import com.microtech.smartshop.enums.UserRole;
+
+public class UserRequest {
+
+    private String name ;
+
+    private String email;
+
+    private String userName;
+
+    private String password ;
+
+    private String address ;
+
+    private int phone ;
+
+    private UserRole role ;
+
+}
