8fb037f5230fbea5eeb47846bd1f63894d88e46d
Moustapha Ndiaye <mstfa.ndiaye23@gmail.com>
2025-11-28 00:52:22 +0100

    feat: update CustomerRepository with findByEmail and findByIsDeletedFalse

--- src/main/java/com/microtech/smartshop/repository/CustomerRepository.java
+++ src/main/java/com/microtech/smartshop/repository/CustomerRepository.java
@@ -1,12 +1,22 @@
-package com.microtech.smartshop.repository ;
+package com.microtech.smartshop.repository;
+
 
 import com.microtech.smartshop.entity.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+    Optional<Customer> findByEmail(String email);
 
+    Page<Customer> findByIsDeletedFalse(Pageable pageable);
+
+    Optional<Customer> findByIdAndIsDeletedFalse(Long id);
+
+    boolean existsByEmail(String email);
 }
