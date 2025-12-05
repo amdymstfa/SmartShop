8c5e6b421b971f58887090b1cb67d54341e65f64
Moustapha Ndiaye <mstfa.ndiaye23@gmail.com>
2025-12-05 16:25:31 +0100

    Add UnauthorizedException for 401 authentication errors

---
+++ src/main/java/com/microtech/smartshop/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.microtech.smartshop.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
