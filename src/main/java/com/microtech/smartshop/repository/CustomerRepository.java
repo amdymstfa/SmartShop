9593d4b84850bc6a7e89b2df186b19fc8065fbfd
Moustapha Ndiaye <mstfa.ndiaye23@gmail.com>
2025-12-06 16:26:24 +0100

    Update CustomerRepository to return lists instead of pages for findByEmail and findByIsDeletedFalse

--- src/main/java/com/microtech/smartshop/repository/CustomerRepository.java
+++ src/main/java/com/microtech/smartshop/repository/CustomerRepository.java
@@ -2,19 +2,19 @@ package com.microtech.smartshop.repository;
 
 
 import com.microtech.smartshop.entity.Customer;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    Optional<Customer> findByEmail(String email);
+    List<Customer> findByEmail(String email);
 
-    Page<Customer> findByIsDeletedFalse(Pageable pageable);
+    List<Customer> findByIsDeletedFalse(Pageable pageable);
 
     Optional<Customer> findByIdAndIsDeletedFalse(Long id);
 
